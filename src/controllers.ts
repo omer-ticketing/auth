2cd4a3200e84288df0e90ec75cc5add3cc9d331b
omer <omerme13@gmail.com>
2024-06-18 22:21:40 +0300

    Feat: creating user

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,39 +1,50 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import { RequestValidationError } from './utils/Errors/requestValidationError';
-import { DatabaseConnectionError } from './utils/Errors/databaseConnectionError';
+import User from "./models/userModel";
+import { BadRequestError } from "./utils/Errors/BadRequestError";
+import { RequestValidationError } from "./utils/Errors/requestValidationError";
 
 export const getCurrentUser = (req: Request, res: Response): void => {
-	res.status(200).json({
-		status: 'success',
-		data: "Hello I'm the current user!"
-	});
+    res.status(200).json({
+        status: "success",
+        data: "Hello I'm the current user!",
+    });
 };
-export const signup = (req: Request, res: Response): void => {	
-	const errors = validationResult(req);
-	
-	if (!errors.isEmpty()) {
-		throw new RequestValidationError(errors.array());
-	}
-	
-	throw new DatabaseConnectionError();
+export const signup = async (req: Request, res: Response): Promise<void> => {
+    const errors = validationResult(req);
 
-	res.status(201).json({
-		status: 'success',
-		data: {
-			user: null
-		}
-	});
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
+	
+    const existingUser = await User.findOne({ email });
+	console.log({existingUser, email, password});
+	
+	if (existingUser) {
+		throw new BadRequestError('Email is already in use!');
+		return;
+	}
+
+    const user = await User.build({ email, password });
+	console.log({user});
+	
+    res.status(201).json({
+        status: "success",
+        data: {
+            user
+        },
+    });
 };
 
 export const signin = (req: Request, res: Response): void => {
-	res.status(200).json({
-		status: 'success',
-	});
+    res.status(200).json({
+        status: "success",
+    });
 };
 
 export const signout = (req: Request, res: Response): void => {
-	res.status(200).json({
-		status: 'success',
-	});
+    res.status(200).json({
+        status: "success",
+    });
 };
