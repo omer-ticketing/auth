24f167e5d86ffbbdc4c97039773aca9f0ecd4651
omer <omerme13@gmail.com>
2024-06-22 22:20:59 +0300

    feat: validate signin

--- src/utils/validators/userValidator.ts
+++ src/utils/validators/userValidator.ts
@@ -1,6 +1,16 @@
 import { body } from "express-validator";
 
+const emailValidation = body("email").isEmail().withMessage("Email must be valid.");
+
 export const validateSignup = [
-    body("email").isEmail().withMessage("Email must be valid."),
-    body("password").trim("").isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters."),
+    emailValidation,
+    body("password")
+        .trim("")
+        .isLength({ min: 4, max: 20 })
+        .withMessage("Password must be between 4 and 20 characters."),
+];
+
+export const validateSignin = [
+    emailValidation,
+    body("password").trim("").notEmpty().withMessage("You must provide a password."),
 ];
