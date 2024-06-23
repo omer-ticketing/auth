fc6432940ef74a01ef9b815efe04e83fb789e711
omer <omerme13@gmail.com>
2024-06-23 19:44:41 +0300

    fix: err handler

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,16 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../utils/errors/customError";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): Response | void => {
     if (err instanceof CustomError) {
-        res.status(err.statusCode).json({
+        return res.status(err.statusCode).json({
             status: "fail",
             errors: err.serializeErrors(),
         });
-		return;
     }
 
-    res.status(400).json({
+    res.status(500).json({
         status: "failed",
         errors: err.message || "Something wend wrong",
     });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import * as userController from '../controllers/userController';
+import * as authMiddlewares from '../middlewares/authMiddlewares';
 import express from 'express';
 
 const router = express.Router();
 
-router.get('/current-user', userController.getCurrentUser);
+router.get('/current-user',authMiddlewares.protect, userController.getCurrentUser);
 
 export default router;
