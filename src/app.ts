581b0cdb3c25ea1e3b812e9526d6e7686aee6ab5
omer <omerme13@gmail.com>
2024-06-29 16:32:05 +0300

    prettify

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
-import express from 'express';
-import 'express-async-errors';
-import cookieSession from 'cookie-session';
-import { NotFoundError } from './utils/errors/notFoundError';
-import { errorHandler } from './middlewares/error';
-import authRouter from './routes/authRoutes';
-import userRouter from './routes/userRoutes';
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError } from "./utils/errors/notFoundError";
+import { errorHandler } from "./middlewares/error";
+import authRouter from "./routes/authRoutes";
+import userRouter from "./routes/userRoutes";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(express.json());
-app.use(cookieSession({
-	signed: false,
-	secure: process.env.NODE_ENV !== 'test'
-}));
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== "test",
+    })
+);
 
-app.use('/api/users', authRouter);
-app.use('/api/users', userRouter);
+app.use("/api/users", authRouter);
+app.use("/api/users", userRouter);
 
-app.all('*', async () => {
-	throw new NotFoundError();
+app.all("*", async () => {
+    throw new NotFoundError();
 });
 app.use(errorHandler);
 
